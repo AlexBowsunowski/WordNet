d6ccc0a1965628d7fcae83cd9034c68a761ddf28
AlexBowsunowski <alexbowsunowski@gmail.com>
2021-08-10 21:02:34 +0300

    SAP done

---
+++ src/sourcecode/SAP.java
@@ -0,0 +1,135 @@
+package sourcecode;
+
+import edu.princeton.cs.algs4.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+import java.util.Stack;
+
+public class SAP {
+
+    public final int INF = Integer.MAX_VALUE;
+    public final Digraph net;
+    // constructor takes a digraph (not necessarily a DAG)
+    public SAP(Digraph G){
+        net = G;
+    }
+
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w){
+        return checkWayFromVtoW(v, w)[0];
+    }
+
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w){
+        return checkWayFromVtoW(v, w)[1];
+    }
+
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w){
+
+        return 0;
+    }
+
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
+        return 0;
+    }
+
+    /**
+     * Find the minimal path. And vertex at which the minimum path is reached will be ancestor.
+     *
+     * @param v - vertex v
+     * @param w - vertex w
+     * @return new arr[2], where arr[0] is shortest ancestral length, arr[1] is common ancestor
+     */
+    int[] checkWayFromVtoW(int v, int w){
+        validateArg(v);
+        validateArg(w);
+
+        // use BreadthFirstDirectedPaths to find all possible paths from the vertex
+        BreadthFirstDirectedPaths way_v = new BreadthFirstDirectedPaths(net, v);
+        BreadthFirstDirectedPaths way_w = new BreadthFirstDirectedPaths(net, w);
+
+
+        // check all vertex and find those that there are in one of the paths of both vertices
+        int current_distance;
+        int min_distance = INF;
+        int ancestor = 0;
+        for(int i = 0; i < net.V() && i != w && i != v; ++i){
+            if(way_v.hasPathTo(i) && way_v.distTo(i) < INF && way_w.hasPathTo(i) && way_w.distTo(i) < INF){
+                current_distance = way_v.distTo(i) + way_w.distTo(i);
+                if(min_distance > current_distance){
+                    min_distance = current_distance;
+                    ancestor = i;
+                }
+            }
+        }
+        return min_distance != INF ? new int[]{min_distance, ancestor} : new int[]{-1, -1};
+    }
+
+    /**
+     * Work with set vertices v and w
+     * Find the minimal path. And vertex at which the minimum path is reached will be ancestor.
+     *
+     * @param v - vertices v
+     * @param w - vertices w
+     * @return new arr[2], where arr[0] is shortest ancestral length, arr[1] is common ancestor
+     */
+    int[] checkWayFromVtoW(Iterable<Integer> v, Iterable<Integer> w){
+        validateArg(v);
+        validateArg(w);
+
+        // use BreadthFirstDirectedPaths to find all possible paths from the vertex
+        BreadthFirstDirectedPaths ways_v = new BreadthFirstDirectedPaths(net, v);
+        BreadthFirstDirectedPaths ways_w = new BreadthFirstDirectedPaths(net, w);
+
+        // check all vertex and find those that there are in one of the paths of both vertices
+        int current_distance;
+        int min_distance = INF;
+        int ancestor = 0;
+        for(int i = 0; i < net.V(); ++i){
+            if(ways_v.hasPathTo(i) && ways_v.distTo(i) < INF && ways_w.hasPathTo(i) && ways_w.distTo(i) < INF){
+                current_distance = ways_v.distTo(i) + ways_w.distTo(i);
+                if(min_distance > current_distance){
+                    min_distance = current_distance;
+                    ancestor = i;
+                }
+            }
+        }
+        return min_distance != INF ? new int[]{min_distance, ancestor} : new int[]{-1, -1};
+    }
+
+
+    //Check the argument with type int for the correctness
+    void validateArg(int v){
+        if(v < 0 || v >= net.V()) throw new IllegalArgumentException("Argument is not in the range 0 to n - 1");
+    }
+
+    //Check the argument with iterable type for the correctness
+    void validateArg(Iterable<Integer> v){
+        if(v == null) throw new IllegalArgumentException("Argument is null");
+        Iterator<Integer> it = v.iterator();
+        while(it.hasNext()){
+            int i = it.next();
+            if(i < 0 && i >= net.V()) throw new IllegalArgumentException("Argument is not in the range 0 to n - 1");
+        }
+    }
+
+    // do unit testing of this class
+    public static void main(String[] args){
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length   = sap.length(v, w);
+            int ancestor = sap.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        }
+    }
+}
---
+++ src/sourcecode/WordNet.java
@@ -0,0 +1,112 @@
+package sourcecode;
+
+
+import java.net.Inet4Address;
+import java.util.HashMap;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.DirectedCycle;
+import edu.princeton.cs.algs4.In;
+
+public class WordNet {
+    private final Digraph wordNet;
+    // sMap contains pairs noun as key and Bag as value that consists of an ID, which noun is contains
+    private final HashMap<String, Bag<Integer>> sMap;
+    // sSet contains pairs noun as key and String as value that consists of synset noun
+    private final HashMap<Integer, String> sSet;
+    private final SAP sap;
+
+    // constructor takes the name of the two input files
+    public WordNet(String synsets, String hypernyms){
+        sMap = new HashMap<String, Bag<Integer>>();
+        sSet = new HashMap<Integer, String>();
+        int count = countSynsets(synsets);
+        wordNet = new Digraph(count);
+        fillWordNet(hypernyms);
+
+        //Check that WordNet is a rooted DAG
+        DirectedCycle check = new DirectedCycle(wordNet);
+        if(check.hasCycle()) throw new IllegalArgumentException("Digraph has cycle.");
+        sap = new SAP(wordNet);
+        //Check that WordNet is singly rooted DAG
+        int root = 0;
+        for(int i = 0; i < count; ++i) if(wordNet.outdegree(i) == 0) root++;
+        if(root != 1) throw  new IllegalArgumentException("Digraph has " + root + "roots.");
+    }
+
+    // returns all WordNet nouns
+    public Iterable<String> nouns(){
+        return sMap.keySet();
+    }
+
+    // is the word a WordNet noun?
+    public boolean isNoun(String word){
+        if(word == null) throw new IllegalArgumentException("Argument is null");
+        return sMap.containsKey(word);
+    }
+
+    // distance between nounA and nounB (defined below)
+    public int distance(String nounA, String nounB){
+
+        return 0;
+    }
+
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // in a shortest ancestral path (defined below)
+    public String sap(String nounA, String nounB){
+        return null;
+    }
+
+
+    /**
+     * Fills sSet, where the key is the ID of the line, and value is the current synset
+     * Fills sMap, where the key is the noun in synset, and value is Bag consisting of id where this noun occurs.
+     * @param synsets
+     * @return the number of synsets
+     */
+    int countSynsets(String synsets) {
+        if (synsets == null) throw new IllegalArgumentException("Argument is null");
+        In in = new In(synsets);
+        int count = 0;
+        while (in.hasNextLine()){
+            count++;
+            String line = in.readLine();
+            String[] parts = line.split(",");
+            int id = Integer.parseInt(parts[0]);
+            sSet.put(id, parts[1]);
+            String[] nouns = parts[1].split(" ");
+            for (String noun : nouns) {
+                if (sMap.get(noun) != null) {
+                    Bag<Integer> bag = sMap.get(noun);
+                    bag.add(id);
+                } else {
+                    Bag<Integer> bag = new Bag<Integer>();
+                    bag.add(id);
+                    sMap.put(noun, bag);
+                }
+            }
+        }
+        return count;
+    }
+
+    //Build WordNet
+    void fillWordNet(String hypernyms){
+        if (hypernyms == null) throw new IllegalArgumentException("Argument is null");
+        In in = new In(hypernyms);
+        while(in.hasNextLine()){
+            String line = in.readLine();
+            String[] parts = line.split(",");
+            int id = Integer.parseInt(parts[0]);
+            for(int i = 1; i < parts.length; ++i){
+                int hypernym = Integer.parseInt(parts[i]);
+                wordNet.addEdge(id, hypernym);
+            }
+        }
+    }
+
+    // do unit testing of this class
+    public static void main(String[] args){
+
+    }
+}
